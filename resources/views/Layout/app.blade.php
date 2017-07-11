b78cec1837c57969bf21d3681bb58cc29ee15695
Kona <alejita-81@hotmail.com>
2017-07-10 21:39:07 -0500

    css
    ...

--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -1,85 +1,222 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Pocket</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
-    <style>
-        body {
-            font-family: 'Lato';
-        }
-
-        .fa-btn {
-            margin-right: 6px;
-        }
-    </style>
-</head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/mesero') }}">
-                    Hola
+<html>
+  <head>
+    <title>
+      hightop - Dashboard
+    </title>
+    <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets/bootstrap.min.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets/font-awesome.min.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\hightop-font.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\isotope.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\jquery.fancybox.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\fullcalendar.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\wizard.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\select2.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\morris.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\datatables.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\datepicker.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\timepicker.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\colorpicker.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\bootstrap-switch.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\bootstrap-editable.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\daterange-picker.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\typeahead.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\summernote.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\ladda-themeless.min.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\social-buttons.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\jquery.fileupload-ui.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\dropzone.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\nestable.css" media="screen" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\pygments.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets/style.css" media="all" rel="stylesheet" type="text/css">
+    <link href="../stylesheets\color\green.css" media="all" rel="alternate stylesheet" title="green-theme" type="text/css">
+    <link href="../stylesheets\color\orange.css" media="all" rel="alternate stylesheet" title="orange-theme" type="text/css">
+    <link href="../stylesheets\color\magenta.css" media="all" rel="alternate stylesheet" title="magenta-theme" type="text/css">
+    <link href="../stylesheets\color\gray.css" media="all" rel="alternate stylesheet" title="gray-theme" type="text/css">
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js" type="text/javascript"></script>
+	<script src="../javascripts\bootstrap.min.js" type="text/javascript"></script>
+	<script src="../javascripts\raphael.min.js" type="text/javascript"></script>
+	<script src="../javascripts\selectivizr-min.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.mousewheel.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.vmap.min.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.vmap.sampledata.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.vmap.world.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.bootstrap.wizard.js" type="text/javascript"></script>
+	<script src="../javascripts\fullcalendar.min.js" type="text/javascript"></script>
+	<script src="../javascripts\gcal.js" type="text/javascript"></script>
+	<script src="../javascripts/jquery.dataTables.min.js" type="text/javascript"></script>
+	<script src="../javascripts/datatable-editable.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.easy-pie-chart.js" type="text/javascript"></script>
+	<script src="../javascripts\excanvas.min.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.isotope.min.js" type="text/javascript"></script>
+	<script src="../javascripts\isotope_extras.js" type="text/javascript"></script>
+	<script src="../javascripts\modernizr.custom.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.fancybox.pack.js" type="text/javascript"></script>
+	<script src="../javascripts\select2.js" type="text/javascript"></script>
+	<script src="../javascripts\styleswitcher.js" type="text/javascript"></script>
+	<script src="../javascripts\wysiwyg.js" type="text/javascript"></script>
+	<script src="../javascripts\typeahead.js" type="text/javascript"></script>
+	<script src="../javascripts\summernote.min.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.inputmask.min.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.validate.js" type="text/javascript"></script>
+	<script src="../javascripts\bootstrap-fileupload.js" type="text/javascript"></script>
+	<script src="../javascripts\bootstrap-datepicker.js" type="text/javascript"></script>
+	<script src="../javascripts\bootstrap-timepicker.js" type="text/javascript"></script>
+	<script src="../javascripts\bootstrap-colorpicker.js" type="text/javascript"></script>
+	<script src="../javascripts\bootstrap-switch.min.js" type="text/javascript"></script>
+	<script src="../javascripts\typeahead.js" type="text/javascript"></script>
+	<script src="../javascripts\spin.min.js" type="text/javascript"></script>
+	<script src="../javascripts\ladda.min.js" type="text/javascript"></script>
+	<script src="../javascripts\moment.js" type="text/javascript"></script>
+	<script src="../javascripts\mockjax.js" type="text/javascript"></script>
+	<script src="../javascripts\bootstrap-editable.min.js" type="text/javascript"></script>
+	<script src="../javascripts\xeditable-demo-mock.js" type="text/javascript"></script>
+	<script src="../javascripts\xeditable-demo.js" type="text/javascript"></script>
+	<script src="../javascripts\address.js" type="text/javascript"></script>
+	<script src="../javascripts\daterange-picker.js" type="text/javascript"></script>
+	<script src="../javascripts\date.js" type="text/javascript"></script>
+	<script src="../javascripts\morris.min.js" type="text/javascript">
+    </script><script src="../javascripts\skycons.js" type="text/javascript"></script>
+	<script src="../javascripts\fitvids.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.sparkline.min.js" type="text/javascript"></script>
+	<script src="../javascripts\dropzone.js" type="text/javascript"></script>
+	<script src="../javascripts\jquery.nestable.js" type="text/javascript"></script>
+	<script src="../javascripts\main.js" type="text/javascript"></script>
+	<script src="../javascripts\respond.js" type="text/javascript"></script>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+  </head>
+  <body class="page-header-fixed bg-1 layout-boxed">
+    <div class="modal-shiftfix">
+      <!-- Navigation -->
+      <div class="navbar navbar-fixed-top scroll-hide">
+        <div class="container-fluid top-bar">
+          <div class="pull-right">
+            <ul class="nav navbar-nav pull-right">
+              <li class="dropdown notifications hidden-xs">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" title="Notificaciones"><span aria-hidden="true" class="hightop-flag"></span>
+                  <div class="sr-only">
+                    Notificaciones
+                  </div>
+                  <p class="counter">
+                    4
+                  </p>
                 </a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">
+                    <div class="notifications label label-info">
+                      New
+                    </div>
+                    <p>
+                      New user added: Jane Smith
+                    </p></a>
 
+                  </li>
+                  <li><a href="#">
+                    <div class="notifications label label-info">
+                      New
+                    </div>
+                    <p>
+                      Sales targets available
+                    </p></a>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                  </li>
+                  <li><a href="#">
+                    <div class="notifications label label-info">
+                      New
+                    </div>
+                    <p>
+                      New performance metric added
+                    </p></a>
 
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/') }}">Home</a></li>
+                  </li>
+                  <li><a href="#">
+                    <div class="notifications label label-info">
+                      New
+                    </div>
+                    <p>
+                      New growth data available
+                    </p></a>
+
+                  </li>
                 </ul>
-
-                <ul class="nav navbar-nav navbar-right">
-
-
-                    @if (Auth::guest())
-                        <li><a href="{{ url('auth/login') }}">Login</a></li>
-                        <li><a href="{{ url('auth/register') }}">Register</a></li>
-                        <li><a href="{{ url('auth/usuario') }}">Usuarios</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->email }} <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('auth/profile')}}"><i class="fa fa-btn fa-default"></i>Perfil</a></li>
-                                <li><a href="{{ url('auth/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
+              </li>
+              <li class="dropdown messages hidden-xs">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" title="mensajes"><span aria-hidden="true" class="hightop-envelope"></span>
+                  <div class="sr-only">
+                    Mensajes
+                  </div>
+                  <p class="counter">
+                    3
+                  </p>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">
+                    <img width="34" height="34" src="images\avatar-male2.png">Could we meet today? I wanted...</a>
+                  </li>
+                  <li><a href="#">
+                    <img width="34" height="34" src="images\avatar-female.png">Important data needs your analysis...</a>
+                  </li>
+                  <li><a href="#">
+                    <img width="34" height="34" src="images\avatar-male2.png">Buy Se7en today, it's a great theme...</a>
+                  </li>
                 </ul>
-            </div>
+              </li>
+              <li class="dropdown user hidden-xs"><a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                <img width="34" height="34" src="images\avatar-male.jpg">John Smith<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">
+                    <i class="fa fa-user"></i>Perfil</a>
+                  </li>
+                  <li><a href="#">
+                    <i class="fa fa-gear"></i>Configuracion</a>
+                  </li>
+                  <li><a href="login1.htm">
+                    <i class="fa fa-sign-out"></i>Salir</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <button class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><div class="logo">
+						<a href="index.html"><img src="images/logo.png" alt="" /></a>
+					</div>
         </div>
-    </nav>
+        <div class="container-fluid main-nav clearfix">
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li>
+                <a href="index.htm" class="current">
+                <span aria-hidden="true" class="fa fa-fw fa-group"></span>Personal</a>
+              </li>
+              <li><a href="social.htm">
+                <span aria-hidden="true" class="fa fa-fw fa-glass"></span>Productos</a>
+              </li>
+              <li class="dropdown"><a data-toggle="dropdown" href="#">
+                <span aria-hidden="true" class="fa fa-fw fa-list"></span>Inventario</a>
+              </li>
+              <li class="dropdown"><a data-toggle="dropdown" href="#">
+                <span aria-hidden="true" class="fa fa-fw fa-usd"></span>Caja</a>
+              </li>
+              <li class="dropdown"><a data-toggle="dropdown" href="#">
+                <span aria-hidden="true" class="fa fa-fw fa-truck"></span>Provedores</a>
+              </li>
+              <li><a href="charts.htm">
+                <span aria-hidden="true" class="fa fa-fw fa-bar-chart-o"></span>Estadisticas</a>
+              </li>
+              <li class="dropdown"><a data-toggle="dropdown"  href="#">
+                <span aria-hidden="true" class="fa fa-fw fa-hospital-o"></span>Mi Negocio</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- End Navigation -->
 
-    <div class="container" style='margin-top: 50px;'>
-        @yield('content')
     </div>
-
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-</body>
+    <div class="">
+              @yield('content')
+    </div>
+  </body>
 </html>
