9e74550706d6dd01ad08fbf434166c945a83cb64
Daniel Rivera <danriveraz@hotmail.com>
2017-08-16 19:45:31 -0500

    Otro cambio :v

--- resources/views/Categoria/edit.blade.php
+++ resources/views/Categoria/edit.blade.php
@@ -4,22 +4,18 @@
           <div class="modal-header" style="BACKGROUND-COLOR: rgb(79,0,85); color:white">
           <button aria-hidden="true" type="button" class="close" data-dismiss="modal" style="color:white">&times;</button>
             <h4 class="modal-title">
-            Registro
+            Editar categoría
             </h4>
           </div>
           <div class="modal-body">
-            <div class="pre-scrollable" >
+            <div class="" >
             <div class="widget-content">
               <div class="form-group">
                 <div class="form-group">
-                  <label for="nombre" class="control-label">
-                    Nombre de la categoría
-                  </label>
-                  <input type="text" name="nombre" class="form-control" value="{{$categoria->nombre}}"/>
+                  <input type="text" name="nombre" placeholder="Nombre" class="form-control" value="{{$categoria->nombre}}" required="true" />
                 </div>
                 <div class="form-grup">
-                    <label for="precio" class="control-label">Precio</label>
-                    <input type="number" min="0" step="any" name="precio" class="form-control" value="{{$categoria->precio}}"/>
+                    <input type="number" placeholder="Precio" min="0" step="any" name="precio" class="form-control" value="{{$categoria->precio}}"/>
                 </div>
               </div>
             </div>
@@ -27,7 +23,6 @@
           </div>
           <div class="modal-footer">
             <button class="btn btn-default" style="BACKGROUND-COLOR: rgb(79,0,85); color:white" >Guardar</button>
-            <button class="btn btn-default-outline" data-dismiss="modal" type="button">Cerrar</button>
           </div>
         {!! Form::close() !!}
     </div>
