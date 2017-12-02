0262dda787f57c0b7911bac1dca44db98779bc7f
felipe1226 <piperomero1226@gmail.com>
2017-12-01 23:33:55 -0500

    Arreglos

--- resources/views/Salario/salario.blade.php
+++ resources/views/Salario/salario.blade.php
@@ -1,59 +1,6 @@
 @extends('Layout.app')
 @section('content')
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>
-      hightop - Dashboard
-    </title>
-    <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700" media="all" rel="stylesheet" type="text/css"><link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/bootstrap.min.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/font-awesome.min.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/hightop-font.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/isotope.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/jquery.fancybox.css" media="all" rel="stylesheet" type="text/css"><link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/fullcalendar.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/wizard.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/select2.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/morris.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/datatables.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/datepicker.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/timepicker.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/colorpicker.css" media="all" rel="stylesheet" type="text/css"><link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/bootstrap-switch.css" media="all" rel="stylesheet" type="text/css"><link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/bootstrap-editable.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/daterange-picker.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/typeahead.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/summernote.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/ladda-themeless.min.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/social-buttons.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/jquery.fileupload-ui.css" media="screen" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/dropzone.css" media="screen" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/nestable.css" media="screen" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/pygments.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/style.css" media="all" rel="stylesheet" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/color/green.css" media="all" rel="alternate stylesheet" title="green-theme" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/color/orange.css" media="all" rel="alternate stylesheet" title="orange-theme" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/color/magenta.css" media="all" rel="alternate stylesheet" title="magenta-theme" type="text/css">
-    <link href="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/stylesheets/color/gray.css" media="all" rel="alternate stylesheet" title="gray-theme" type="text/css">
-    <script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript">
-    </script><script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/bootstrap.min.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/raphael.min.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/selectivizr-min.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.mousewheel.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.vmap.min.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.vmap.sampledata.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.vmap.world.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.bootstrap.wizard.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/fullcalendar.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/gcal.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.dataTables.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/datatable-editable.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.easy-pie-chart.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/excanvas.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.isotope.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/isotope_extras.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/modernizr.custom.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.fancybox.pack.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/select2.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/styleswitcher.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/wysiwyg.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/typeahead.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/summernote.min.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.inputmask.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.validate.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/bootstrap-fileupload.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/bootstrap-datepicker.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/bootstrap-timepicker.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/bootstrap-colorpicker.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/bootstrap-switch.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/typeahead.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/spin.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/ladda.min.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/moment.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/mockjax.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/bootstrap-editable.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/xeditable-demo-mock.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/xeditable-demo.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/address.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/daterange-picker.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/date.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/morris.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/skycons.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/fitvids.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.sparkline.min.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/dropzone.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/jquery.nestable.js" type="text/javascript"></script><script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/main.js" type="text/javascript"></script>
-    <script src="../../../xampp/htdocs/PocketByR/pantallas principales/Plantilla para el Bar/javascripts/respond.js" type="text/javascript"></script>
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
-  </head>
-  <body class="page-header-fixed bg-1">
+
     <div class="modal-shiftfix">
       <!-- Navigation --><!-- End Navigation -->
       <div class="container-fluid main-content">
@@ -235,5 +182,4 @@
       </div>
   </div>
     <div class="style-selector"></div>
-  </body>
-</html>
+  
