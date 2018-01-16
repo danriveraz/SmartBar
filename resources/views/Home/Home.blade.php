08507a9eee6d9352b338d735018320573ef01e8b
konaizumi <alejita-81@hotmail.com>
2018-01-16 12:26:16 -0500

    Chat

--- resources/views/Home/Home.blade.php
+++ resources/views/Home/Home.blade.php
@@ -396,6 +396,17 @@
       }
     };
 </script>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '3a7eda45afb7eb3d94232e55012471c2d2a9a883';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
 
 </body>
 
