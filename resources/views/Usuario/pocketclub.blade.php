a325808e436a02ceec47882eec01c8536e3893ba
Daniel Rivera <danriveraz@hotmail.com>
2018-01-24 12:06:10 -0500

    Pocketclub listo (para pagos toca estar en el host)

--- resources/views/Usuario/pocketclub.blade.php
+++ resources/views/Usuario/pocketclub.blade.php
@@ -8,61 +8,77 @@
   <div class="block-content text-center">
     <h2>Membresia PocketClub</h2>
     <div class="row plans cards-basic">
-      <div class="col plan" id="unica">
+      <div class="col plan bg-gray-white" id="unica" style="display: none;">
           <h2 class="plan-title">ÚNICA</h2>
           <ul>
-              <li>Hasta 7 Empleados</li>
-              <li>Asesoria de Lunes a Viernes(✓)</li>
-              <li>1 sólo negocio</li>
-              <li>Promociones Únicas SmartShop</li>
-              <li>Uso del programa por 30 días</li>
-              <li>Información segura hasta por 90 días</li>
-              <li>Acceso al 100% de las utilidades SmartBar</li>
-              <li>No aplica mes GRATIS</li>
-              <li>0% de ahorro</li>
-              
+            <li>Hasta 20 empleados</li>
+            <li>Asesoria de Lunes a Sábado(✓)</li>
+            <li>Hasta 2 negocios</li>
+            <li>Promociones Únicas y especiales SmartShop</li>
+            <li>Uso del programa por 6 meses</li>
+            <li>Información segura hasta por 1 año</li>
+            <li>Acceso al 100% de las utilidades SmartBar</li>
+            <li>El mes 6 es GRATIS</li>
+            <li>Ahorra hasta  $ 100.000 Cop</li>
+            <h3 class="plan-price"> $ 99.900 COP/Mes</h3>
+            <p> 10% descuento en pago trimestral. </p>
           </ul>
-          <h3 class="plan-price"> $ 99.900 COP/Mes</h3>
-          <p>
-            10% descuento en pago trimestral.
-          </p>
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="YEUTK66UGQZ8W">
+            <table align="center">
+              <tr>
+                <td>
+                  <input type="hidden" name="on0" value="Duracion">Duracion
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <select name="os0" class="form-control">
+                    <option value="Mensual">Mensual </option>
+                    <option value="Bimestral">Bimestral </option>
+                    <option value="Trimestral">Trimestral </option>
+                </select> 
+                </td>
+              </tr>
+            </table>
+            <br>
+            <button class="btn btn-bitbucket" border="0" name="submit" alt="PayPal, la forma rápida y segura de pagar en Internet."> 
+              Suscribirse
+            </button> 
+          </form>
       </div>
-      <div class="col plan bg-gray-white" id="especial">
+      <div class="col plan bg-gray-white" id="especial" style="display: none;">
         <h2 class="plan-title">ESPECIAL</h2>
           <ul>
-              <li>Hasta 20 Empleados</li>
-              <li>Asesoria de Lunes a Sábado (✓)</li>
-              <li>Hasta 3 negocios</li>
+              <li>Hasta 20 empleados</li>
+              <li>Asesoria de Lunes a Sábado(✓)</li>
+              <li>Hasta 2 negocios</li>
               <li>Promociones Únicas y especiales SmartShop</li>
               <li>Uso del programa por 6 meses</li>
               <li>Información segura hasta por 1 año</li>
               <li>Acceso al 100% de las utilidades SmartBar</li>
               <li>El mes 6 es GRATIS</li>
-              <li>Ahorra hasta $ 100.000 Cop</li>
-              
+              <li>Ahorra hasta  $ 100.000 Cop</li>
+              <h3 class="plan-price"> $ 499.500 COP</h3>
+              <li>10% descuento en pago anual.</li>
           </ul>
-          <h3 class="plan-price">$ 499.500 COP/Mes</h3>
-          <p>
-            10% descuento en pago anual.
-          </p>
       </div>
-      <div class="col plan" id="elite">
+      <div class="col plan bg-gray-white" id="elite" style="display: none;">
           <h2 class="plan-title">ÉLITE</h2>
           <ul>
               <li>Empleados infinitos</li>
               <li>Asesoria de 24/7, 365 días al año (✓)</li>
-              <li>Hasta 10 Negocios</li>
+              <li>Hasta 4 negocios</li>
               <li>Todas las promociones SmartShop</li>
               <li>Uso del programa por 3 años</li>
               <li>Información segura hasta por 5 años</li>
               <li>Acceso al 100% de las utilidades SmartBar</li>
               <li>El mes 1, 12,24 y 36 son GRATIS</li>
               <li>Ahorra hasta $ 400.000 Cop</li>
+              <h3 class="plan-price">$ 2.998.800 COP</h3>
+              <p>Descuento en membresia de $ 198.000 Cop</p>
           </ul>
-          <h3 class="plan-price">$ 2.998.800 COP/Mes</h3>
-          <p>
-            Descuento en membresia de $ 198.000 Cop
-          </p>
       </div>
     </div>
     <p><img class="plan-payment" src="../../../assetsNew/images/logos-payment-col.png" alt="Métodos de pago"/></p>
@@ -116,14 +132,11 @@ section{
   var JSONplan = eval(<?php echo json_encode($plan); ?>);
   $(document).ready(function(){
     if(JSONplan == 1){
-      document.getElementById('especial').style.display = 'none';
-      document.getElementById('elite').style.display = 'none';
+      document.getElementById('unica').style.display = 'block';      
     }else if(JSONplan == 2){
-      document.getElementById('unica').style.display = 'none';
-      document.getElementById('elite').style.display = 'none';
+      document.getElementById('especial').style.display = 'block';
     }else{
-      document.getElementById('unica').style.display = 'none';
-      document.getElementById('especial').style.display = 'none';
+      document.getElementById('elite').style.display = 'block';
     }
   });
 </script>
